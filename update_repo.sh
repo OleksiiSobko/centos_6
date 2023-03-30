d4288efce5abb280c261b5bf4b3f426616937072
Sobko Oleksii <53451019+OleksiiSobko@users.noreply.github.com>
2023-03-30 20:03:38 +0300

    Update update_repo.sh

--- update_repo.sh
+++ update_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 unzip yum.zip
-rsync -vaP ./yum.repos.d/* /etc/yum.repos.d/
+rsync -vaP /tmp/centos_repo/yum.repos.d/* /etc/yum.repos.d/
 sed -i 's/^\([^#]\)/#\1/g' /etc/yum.repos.d/bintray-sbt-rpm.repo
 sed -i 's/^\([^#]\)/#\1/g' /etc/yum.repos.d/CentOS-SCLo-scl-rh.repo 
 yum clean all
